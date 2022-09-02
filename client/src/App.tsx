cb8e198150b72cc8014f24a2d7e1319c5cb33d65
jake-the-cake <askjake331@gmail.com>
2022-09-01 21:11:13 -0400

    add some routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
-import React, { Context, useContext } from 'react'
+import React, { Context, createContext } from 'react'
+import { Dashboard } from './pages/Dashboard';
+import { Route, Routes, Link, BrowserRouter } from 'react-router-dom'
 
-const UserContext: Context<any>= React.createContext(null)
+const UserContext: Context<any> = createContext(null)
 let user = 'papa'
 
 const App = () => {
   return (
     <UserContext.Provider value={{user:user + 'n'}}>
-      <div className="App">
-        App
-      </div>
+      <BrowserRouter>
+        <nav className='header__container'>
+          Navbar
+          <Link to='/about'>About</Link>
+          <Link to='/'>Home</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/about' element={<>About</>} />
+        </Routes>
+      </BrowserRouter>
     </UserContext.Provider>
   );
 }
